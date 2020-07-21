4c2e78456025b1b5c38123605e9940c5c5508c22
Ashutosh Singh <ashutoshsinghks@gmail.com>
2020-07-21 13:32:07 +0530

    satisfied

--- js/custom.js
+++ js/custom.js
@@ -216,7 +216,7 @@ function setData(data, c) {
 
         });
     }
-
+    document.getElementById('start-text').style.display = 'none';
     mainElement.innerHTML = output;
     setEventListener();
 }
